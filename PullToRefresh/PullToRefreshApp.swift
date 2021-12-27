532536880a8230036d01d038a96c2089abfc78d6
maxkalik <maxkalik@gmail.com>
2021-12-27 09:02:17 -0800

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Maksim Kalik on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
