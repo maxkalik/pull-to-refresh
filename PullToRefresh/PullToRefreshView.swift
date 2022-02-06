56398fcc69722e9dd0818472f3d74496e9cda9f0
maxkalik <maxkalik@gmail.com>
2022-02-06 00:22:33 -0800

    [TAS-102]: Some Feature Task 02; Structure

---
+++ PullToRefresh/PullToRefreshView.swift
@@ -0,0 +1,48 @@
+//
+//  PullToRefreshView.swift
+//  PullToRefresh
+//
+//  Created by Maksim Kalik on 2/6/22.
+//
+
+import SwiftUI
+
+struct PullToRefreshView: View {
+    
+    @State var arrayOfData: [String] = [String](repeating: "List item number: ", count: 7)
+        .enumerated()
+        .map { "\($1) \($0)" }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("Pull to Refresh")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .padding()
+            .background(Color.black.ignoresSafeArea(.all, edges: .top))
+
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    ForEach(arrayOfData, id: \.self) { value in
+                        HStack {
+                            Text(value)
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                }
+                .background(Color.white)
+            }
+            .pullToRefreshable {
+                arrayOfData.append("Added List Item \(arrayOfData.count)")
+            }
+            .padding(20)
+            
+        }
+        .background(Color.black.opacity(0.06).ignoresSafeArea())   
+    }
+}
